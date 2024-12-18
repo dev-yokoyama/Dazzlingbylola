4e284faa60657be0d75e666cb1972bd41faaee82
Pedro <pedro.yokomarques@gmail.com>
2024-12-18 13:01:10 -0300

    Dazzling by lola

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+      delay: 3000, // Tempo entre os slides (em milissegundos)
+      disableOnInteraction: false, // Continua o autoplay após interação
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 'auto', /* Define largura personalizada */
+    spaceBetween: 10,
+  });
+  const meuCarrossel = new Swiper(".meu-carrossel", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: false, // Não repete os slides
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2, // 2 slides para tablets
+    },
+    1024: {
+      slidesPerView: 4, // 3 slides para desktops
+    },
+  },
+});
